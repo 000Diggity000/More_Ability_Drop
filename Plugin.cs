e9c40e90fb08335e97336d381af314d9862bd145
000Diggity000 <116099377+000Diggity000@users.noreply.github.com>
2024-09-24 18:53:01 +0200

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -12,33 +12,45 @@ using UnityEngine;
 
 namespace More_Ability
 {
-    [BepInPlugin("com.000diggity000.More_Ability_Drop", "More Ability Drop", "1.0.0")]
+    [BepInPlugin("com.000diggity000.More_Ability_Drop", "More Ability Drop", "1.1.0")]
     public class Plugin : BaseUnityPlugin
     {
         public static int abilityCount = 0;
+        public static bool random = false;
         internal static ConfigFile config = new ConfigFile(Path.Combine(Paths.ConfigPath, "More_Ability_Drop_Config.cfg"), true);
         internal static ConfigEntry<int> AbilityConfig;
+        internal static ConfigEntry<bool> RandomConfig;
         private void Awake()
         {
             Harmony harmony = new Harmony("com.000diggity000.More_Ability_Drop");
             harmony.PatchAll(typeof(Patches));
             AbilityConfig = config.Bind("More Ability Drop", "Abilities", 3, "Minimum is 1");
-            if (AbilityConfig.Value <= 1)
+            RandomConfig = config.Bind("More Ability Drop", "Random", false, "If true, the abilities dropped are random. Only abilities from the ones being used can spawn.");
+            if (AbilityConfig.Value <= 0)
             {
-                AbilityConfig.Value = 1;
+                AbilityConfig.Value = 0;
             }
             abilityCount = AbilityConfig.Value;
+            random = RandomConfig.Value;
         }
     }
     public class Patches
     {
         [HarmonyPatch(typeof(SlimeController), "DropAbilities")]
         [HarmonyPrefix]
-        public static void DropAbilities_Patch(SlimeController __instance, ref AbilityReadyIndicator[] ___AbilityReadyIndicators, ref NamedSpriteList ___abilityIconsFull, ref NamedSpriteList ___abilityIconsDemo, ref DynamicAbilityPickup ___abilityPickupPrefab)
+        public static bool DropAbilities_Patch(SlimeController __instance, ref AbilityReadyIndicator[] ___AbilityReadyIndicators, ref NamedSpriteList ___abilityIconsFull, ref NamedSpriteList ___abilityIconsDemo, ref DynamicAbilityPickup ___abilityPickupPrefab)
         {
             if (!GameSession.IsInitialized() || GameSessionHandler.HasGameEnded() || __instance.abilities.Count <= 0)
             {
-                return;
+                return false;
+            }
+            PlayerHandler.Get().GetPlayer(__instance.playerNumber);
+            for (int i = 0; i < ___AbilityReadyIndicators.Length; i++)
+            {
+                if (___AbilityReadyIndicators[i] != null)
+                {
+                    ___AbilityReadyIndicators[i].InstantlySyncTransform();
+                }
             }
             int num = Settings.Get().NumberOfAbilities - 1;
             while (num >= 0 && (num >= ___AbilityReadyIndicators.Length || ___AbilityReadyIndicators[num] == null))
@@ -47,24 +59,32 @@ namespace More_Ability
             }
             if (num < 0)
             {
-                return;
-            }
-            Vec2 launchDirection = Vec2.NormalizedSafe(Vec2.up + new Vec2(Updater.RandomFix((Fix)(-0.3f), (Fix)0.3f), (Fix)0L));
-            Sprite primarySprite = ___AbilityReadyIndicators[num].GetPrimarySprite();
-            NamedSprite namedSprite = ___abilityIconsFull.sprites[___abilityIconsFull.IndexOf(primarySprite)];
-            if (namedSprite.associatedGameObject == null)
-            {
-                namedSprite = ___abilityIconsDemo.sprites[___abilityIconsDemo.IndexOf(primarySprite)];
-            }
-            for (int i = 2; i <= Plugin.abilityCount; i++)
-            {
-                
-                DynamicAbilityPickup dynamicAbilityPickup = FixTransform.InstantiateFixed<DynamicAbilityPickup>(___abilityPickupPrefab, __instance.body.position);
-                
-                
-                dynamicAbilityPickup.InitPickup(namedSprite.associatedGameObject, primarySprite, launchDirection);
+                return false;
             }
             
+            Vec2 launchDirection = Vec2.NormalizedSafe(Vec2.up + new Vec2(Updater.RandomFix((Fix)(-0.3f), (Fix)0.3f), (Fix)0L));
+            
+            for (int i = 1; i <= Plugin.abilityCount; i++)
+            {
+                int rnd = UnityEngine.Random.Range(0, (int)___AbilityReadyIndicators.Length);
+                if (Plugin.random)
+                {
+                    Sprite primarySprite = ___AbilityReadyIndicators[rnd].GetPrimarySprite();
+                    NamedSprite namedSprite = ___abilityIconsFull.sprites[___abilityIconsFull.IndexOf(primarySprite)];
+                    DynamicAbilityPickup dynamicAbilityPickup = FixTransform.InstantiateFixed<DynamicAbilityPickup>(___abilityPickupPrefab, __instance.body.position);
+                    dynamicAbilityPickup.InitPickup(namedSprite.associatedGameObject, primarySprite, launchDirection);
+                }
+                else
+                {
+                    Sprite primarySprite = ___AbilityReadyIndicators[num].GetPrimarySprite();
+                    NamedSprite namedSprite = ___abilityIconsFull.sprites[___abilityIconsFull.IndexOf(primarySprite)];
+                    DynamicAbilityPickup dynamicAbilityPickup = FixTransform.InstantiateFixed<DynamicAbilityPickup>(___abilityPickupPrefab, __instance.body.position);
+                    dynamicAbilityPickup.InitPickup(namedSprite.associatedGameObject, primarySprite, launchDirection);
+                }
+                
+                
+            }
+            return false;
         }
     }
 }
